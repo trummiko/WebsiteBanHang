86e40744f1dfc3acc1becd722732d4946011d038
trummiko <ngocthang230402@gmail.com>
2024-06-23 06:33:02 +0700

    phan quyen thanh cong

--- src/main/java/Java_Project/WebsiteBanHang/Model/User.java
+++ src/main/java/Java_Project/WebsiteBanHang/Model/User.java
@@ -8,7 +8,6 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
